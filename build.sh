4362256ed13acf7d2e33de715201639ff6971b9e
Dmitriiplastinin-1231 <dimanplastinen@gmail.com>
2025-03-05 17:16:39 +0300

    all

--- build.sh
+++ build.sh
@@ -4,3 +4,24 @@ SOURCE_FOLDER="Hw"
 
 cmake -G ${BUILD_TYPE} ../${SOURCE_FOLDER}
 cmake --build .
+
+mkdir build
+exclude_dirs="lib CMakeFiles unit_test_HoareSort build"
+
+for dir in */; do
+    dir=${dir%/}
+    exclude=0
+    for exclude_dir in $exclude_dirs; do
+        if [ "$dir" = "$exclude_dir" ]; then
+            exclude=1
+            break
+        fi
+    done
+
+    if [ $exclude -eq 0 ]; then
+        mkdir "build/$dir"
+        mv "./$dir/$dir" "./build/$dir/$dir"
+    fi
+done
+
+ctest -j N --output-on-failure
